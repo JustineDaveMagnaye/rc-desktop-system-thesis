baad5412ca6dc036f3fcea4a00dc359819285bdb
JustineDaveMagnaye <157860693+JustineDaveMagnaye@users.noreply.github.com>
2024-11-10 15:07:34 +0800

    s

---
+++ tito-authenticator-application/src/main/java/com/system/demo/appl/model/timeRecord/TimeRecord.java
@@ -0,0 +1,67 @@
+package com.system.demo.appl.model.timeRecord;
+
+import java.sql.Timestamp;
+
+public class TimeRecord {
+
+    private int id;
+    private String employeeNumber;
+    private Timestamp timeIn;
+    private Timestamp timeOut;
+    private double totalHours;
+    private String createdAt;
+
+    // Constructor
+    public TimeRecord() {
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Timestamp getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(Timestamp timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public Timestamp getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut(Timestamp timeOut) {
+        this.timeOut = timeOut;
+    }
+
+    public double getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(Double totalHours) {
+        this.totalHours = totalHours;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
